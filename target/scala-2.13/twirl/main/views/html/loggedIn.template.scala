96e8a697a7244b32a05d1e7123e23a71eab4b3ba
Andrew Kleinman <andrewekleinman@gmail.com>
2021-08-02 17:12:51 -0700

    revert

--- target/scala-2.13/twirl/main/views/html/loggedIn.template.scala
+++ target/scala-2.13/twirl/main/views/html/loggedIn.template.scala
@@ -26,8 +26,11 @@ object loggedIn extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.H
 Seq[Any](format.raw/*1.20*/("""
 
   """),_display_(/*3.4*/main("log")/*3.15*/ {_display_(Seq[Any](format.raw/*3.17*/("""
-  """),format.raw/*4.3*/("""you are logged in as """),_display_(/*4.25*/username),format.raw/*4.33*/("""
-  """)))}),format.raw/*5.4*/("""
+    """),format.raw/*4.5*/("""<br>
+    <center>
+  you are logged in as """),_display_(/*6.25*/username),format.raw/*6.33*/("""
+    """),format.raw/*7.5*/("""</center>
+  """)))}),format.raw/*8.4*/("""
 """))
       }
     }
@@ -45,9 +48,9 @@ Seq[Any](format.raw/*1.20*/("""
               /*
                   -- GENERATED --
                   SOURCE: app/views/loggedIn.scala.html
-                  HASH: 9838b75b8948244b9757e1d39b0854bb086cbfec
-                  MATRIX: 732->1|845->19|877->26|896->37|935->39|965->43|1013->65|1041->73|1075->78
-                  LINES: 21->1|26->1|28->3|28->3|28->3|29->4|29->4|29->4|30->5
+                  HASH: 8d9aeb2c3575405abe30a8fc5cf96aebeea1bab1
+                  MATRIX: 732->1|845->19|877->26|896->37|935->39|967->45|1037->89|1065->97|1097->103|1140->117
+                  LINES: 21->1|26->1|28->3|28->3|28->3|29->4|31->6|31->6|32->7|33->8
                   -- GENERATED --
               */
           
